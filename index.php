9e8e305bc0e36e66176cf877b0e80dc6cb547b7b
Shestakov Sergey <shestakovpost@gmail.com>
2015-05-20 11:17:06 +1000

    dz3

--- index.php
+++ index.php
@@ -3,39 +3,32 @@ error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
 ini_set('display_errors', 1 );
 header('Content-type: text/html; charset=utf-8');
 
-//$name = "Сергей";
-//$age = 26;
-//echo "Меня зовут $name и мне $age лет.";
+$date=array();
+    $date[]=  rand(time(), 1);
+    $date[]=  rand(time(), 1);
+    $date[]=  rand(time(), 1);
+    $date[]=  rand(time(), 1);
+    $date[]=  rand(time(), 1);
+    print_r($date);
+    sort($date);
+    echo date('d.m.Y',$date[0]).'<br>';
+    
+$s=array();
+$x=array();
+    foreach ($date as $key => $value) {
+        echo date('d.m.Y',$value).'<br>';
+        $x[]=date('d',$value);
+        $s[]=date('m',$value);    
+    } 
+sort($x);
+sort($s);
+echo "наименьший день в массиве ".$x[0].'<br>';
+echo "наибольший месяц в массиве ".$s[4].'<br>';
 
-define('GOROD', "Koms");
-echo GOROD.'<br>';
-//define('GOROD', "dddd");
+$selected=array_pop($date);
+echo 'посдедний элемент массива: '.date('d.m.Y H:i:s',$selected).'<br>';
 
-$book=array();
-    $book['title']='Наследие';
-    $book['author']='Тармашев';
-    $book['pages']='100';
-echo "Недавно я прочитал книгу $book[title] , написанную автором $book[author], я осилил все $book[pages] страниц, мне она очень понравилась <br>";
-
-    $book1=array();
-        $book1['title1']='Метро';
-        $book1['author1']='Глуховский';
-        $book1['pages1']='200';
-    $book2=array();
-        $book2['title2']='РАБ';
-        $book2['author2']='Минаев';
-        $book2['pages2']='300';
-        
-$books=array($book1, $book2);
-echo "Недавно я прочитал книги ".$books['0']["title1"]." и ".$books['1']["title2"]." написанные соответственно авторами ".$books['0']["author1"]." и ".$books['1']["author2"];
-echo ", я осилил в сумме ";
-echo $books['0']["pages1"]+$books['1']["pages2"]." страниц, не ожидал от себя подобного" ;
-
-
-
-
-
-
-
-
-    ?>
+echo 'время на сервере: '.date('d.m.Y H:i:s',time()).'<br>';
+date_default_timezone_set('America/New_York');
+echo 'время в Нью-Йорке: '.date('d.m.Y H:i:s',time()).'<br>';
+//?>
